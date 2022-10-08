245a842db1e4fc8a10a64573f4b7e8681cd4e8b2
Rehan Arshad <rehanarshad94@yahoo.com>
2022-10-07 20:24:26 -0500

    q

--- passport.js
+++ passport.js
@@ -23,10 +23,10 @@ passport.use(new LocalStrategy({
             console.log('incorrect username');
             return callback(null, false, {message: 'Incorrect username and password'});
         } 
-        if (!user.validatePassword(password)) {     // Additional callback code to validate any password a user enters
-            console.log('incorrect password');
-            return callback(null, false, {message: 'Incorrect password.'});
-        }
+        // if (!user.validatePassword(password)) {     // Additional callback code to validate any password a user enters
+        //     console.log('incorrect password');
+        //     return callback(null, false, {message: 'Incorrect password.'});
+        // }
         console.log('finished')
         return callback(null, user);
     });
