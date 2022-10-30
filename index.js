2bffcbed27c50e481c54ad37b4c61b7ecc8cbc4e
Rehan Arshad <rehanarshad94@yahoo.com>
2022-10-30 15:52:18 -0500

    update

--- index.js
+++ index.js
@@ -27,23 +27,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Allows access for all domains
-// const cors = require('cors');
-// app.use(cors());
+const cors = require('cors');
+app.use(cors());
 
 // Only allows selected domains to access the information 
-const cors = require('cors');
-let allowedOrigins = ['https://localhost:1234'];
+// const cors = require('cors');
+// let allowedOrigins = ['https://localhost:1234'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 
